e0d8e8b8abf83b86a122eb585f423461fdf98bfc
Rizwan Zaheer <rizwanzaheerr@gmail.com>
2018-12-23 22:23:40 +0500

    add delete user func

--- schema/schema.js
+++ schema/schema.js
@@ -74,6 +74,15 @@ const mutation = new GraphQLObjectType({
       resolve(parentValue, { firstName, age }) {
         return axios.post(`http://localhost:3000/users/`, { firstName, age }).then(res => res.data);
       }
+    },
+    deleteUser: {
+      type: UserType,
+      args: {
+        id: { type: new GraphQLNonNull(GraphQLString) }
+      },
+      resolve(parentValue, { id }) {
+        return axios.delete(`http://localhost:3000/users/${id}`).then(res => res.data);
+      }
     }
   }
 });
